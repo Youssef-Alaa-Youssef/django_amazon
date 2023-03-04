ba71d772226e41f086be5eaf9ceb4f1de4f7ecab
Youssef Alaa <yalaa1324@gmail.com>
2023-03-04 09:05:44 +0200

    Add ,Delete ,Update,show Products

--- amazon/settings.py
+++ amazon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
+    'catetegory.apps.CatetegoryConfig'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- products/models.py
+++ products/models.py
@@ -1,19 +1,15 @@
 from django.db import models
-
+from catetegory.models import Category
 # Create your models here.
 
 
-# class Category(models.Model):
-#     name = models.CharField(max_length=100)
-
-#     def __str__(self):
-#         return self.name
 class Product(models.Model):
     name = models.CharField(max_length=15)
     date = models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length=100)
     image =models.ImageField(upload_to='images/', null=True, blank=True)
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True,
+                                 related_name='category_posts')
 
     def __str__(self):
         return self.name
