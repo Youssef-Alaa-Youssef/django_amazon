3dd6c3b536400c636af80da714fd4a9ac1e95715
Youssef Alaa <yalaa1324@gmail.com>
2023-03-05 00:04:17 +0200

    convert all function based view into generic class based view

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,10 +6,14 @@ from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 
 urlpatterns = [
-    path('signup/',views.signUp,name="signUp"),
+    path('signup/',views.signUp.as_view(),name="signUp"),
+    # path('signup/',views.signUp,name="signUp"),
+    path('login/',views.Login.as_view(),name="login"),
     path('logout/',auth_views.LogoutView.as_view(),name="logout"),
-    path('login/',auth_views.LoginView.as_view(template_name ="login.html"),name="login"),
-    path('setting/changepassword/',auth_views.PasswordChangeView.as_view(template_name ="changepassword.html",success_url=reverse_lazy('login')),name="change.password"),
+    # path('login/',auth_views.LoginView.as_view(template_name ="login.html"),name="login"),
+    # path('setting/changepassword/',auth_views.PasswordChangeView.as_view(template_name ="changepassword.html",success_url=reverse_lazy('login')),name="change.password"),
+    path('setting/changepassword/',views.ChangePassword.as_view(),name="change.password"),
+
     # path('setting/changepassworddone/',auth_views.PasswordChangeDoneView.as_view(template_name ="changepassworddone.html"),name="change.password.done"),
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- accounts/views.py
+++ accounts/views.py
@@ -2,16 +2,49 @@ from django.shortcuts import render,redirect
 
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.views.generic import View
+from django.contrib.auth.views import LoginView,PasswordChangeView
 from .forms import sigup
 # Create your views here.
 
 
-def signUp(request):
-    form = sigup()
-    if request.method == "POST":
-        form = sigup(request.POST)
-        if form.is_valid():
-            user = form.save()
-            auth_login(request,user)
-            return redirect('home')
-    return render(request,"signup.html",{"signup":form})
+# def signUp(request):
+#     form = sigup()
+#     if request.method == "POST":
+#         form = sigup(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             auth_login(request,user)
+#             return redirect('home')
+#     return render(request,"signup.html",{"signup":form})
+
+
+# class signUp(View):
+#     def post(self,request):
+#         form = sigup(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             auth_login(request,user)
+#             return redirect('home')
+#         return render(request,"signup.html",{"signup":form})
+
+
+class signUp(CreateView):
+    model = auth_login
+    form_class = sigup
+    template_name = "signup.html"
+    success_url = '/login'
+
+
+
+class Login(LoginView):
+    template_name = "login.html"
+    success_url = '/home'
+
+
+
+class ChangePassword(PasswordChangeView):
+    template_name ="changepassword.html"
+    success_url = '/login'
